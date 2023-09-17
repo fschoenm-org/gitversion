ddb9f7c619bf6bbd64e47eed56bfc4f3d1f1c228
Frank Schoenmann <fschoenm@users.noreply.github.com>
2023-09-17 16:27:58 +0200

    Debug output

--- gitversion.py
+++ gitversion.py
@@ -9,3 +9,4 @@ json_output = json.loads(output.decode("utf-8"))
 with open(os.environ["GITHUB_OUTPUT"], "a") as file:
     for key, value in json_output.items():
         print(f"{key}={value}", file=file)
+        print(f"{key}={value}")

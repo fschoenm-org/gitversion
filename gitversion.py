b243335717c727a7cad562614696af7135d7b23a
Frank Schoenmann <fschoenm@users.noreply.github.com>
2023-09-17 16:20:07 +0200

    Call gitversion with workspace path

--- gitversion.py
+++ gitversion.py
@@ -1,8 +1,9 @@
 import json
 import os
 import subprocess
+import sys
 
-output = subprocess.check_output(["gitversion"])
+output = subprocess.check_output(["gitversion", sys.argv[1]])
 json_output = json.loads(output.decode("utf-8"))
 
 with open(os.environ["GITHUB_OUTPUT"], "a") as file:
